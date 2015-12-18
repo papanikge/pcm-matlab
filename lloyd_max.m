1659b50a5fff3a7437b7d04af31021929a01cc58
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2015-12-18 16:20:44 +0200

    fix lloyd-max bugs; both algs work

--- lloyd_max.m
+++ lloyd_max.m
@@ -37,7 +37,7 @@ centers = flip(centers);
 centers = [min_value centers max_value];
 
 % D is the Distortion log.
-D = [0 temp];
+D = [0 1];
 
 % Main algorithm loop.
 k = 2;
@@ -51,7 +51,7 @@ while abs(D(k) - D(k-1)) >= eps
     % Calculating the quantization zones.
     T = [];
     T(1) = min_value;
-    for i=2:(length(centers)-1)
+    for i=2:(length(centers)-2)
         T(i) = (centers(i) + centers(i+1))/2;
     end
     T(i+1) = max_value;
@@ -60,7 +60,7 @@ while abs(D(k) - D(k-1)) >= eps
     for i=1:s
         % Iterating over the zones to find the correct one.
         for j=1:(length(T)-1)
-            if T(j) < x(i) && x(i) < T(j+1)
+            if T(j) < x(i) && x(i) <= T(j+1)
                 % The result.
                 xq(i) = j;
                 % We need to calculate the mean distance from the center.
@@ -70,6 +70,14 @@ while abs(D(k) - D(k-1)) >= eps
                 counted(j)   = counted(j) + 1;
             end
         end
+        % Edge case of a data point directly on the min bound.
+        if x(i) == T(1)
+            % Almost the same stuff as above.
+            xq(i) = 1;
+            total = total + abs(centers(2) - x(i));
+            cond_mean(1) = cond_mean(1) + x(i);
+            counted(1)   = counted(1) + 1;
+        end
     end
     avg_distortion = total/s;
 
@@ -78,8 +86,10 @@ while abs(D(k) - D(k-1)) >= eps
     k = k + 1;
 
     % Finding the new centers, since that's what we need to get the zones.
-    for j=1:(length(cond_mean)-1)
-        centers(j) = cond_mean(j)/counted(j);
+    for j=2:(length(centers)-1)
+        if counted(j-1) ~= 0
+            centers(j) = cond_mean(j-1)/counted(j-1);
+        end
     end
 end
 
@@ -87,5 +97,9 @@ end
 % Now D(i) is the mean distortion of the i-th iteration.
 D(1) = [];
 D(2) = [];
+
+% Fix the `centers` vector too.
+centers(1) = [];
+centers(length(centers)) = [];
 
 fprintf('Successfully exited after %d iterations.\n', k-2);
